478a0fd230d676a1461d4aa3254f8ba3edec05b7
JabimSK <6357@holbertonstudents.com>
2023-08-14 16:58:08 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,12 +7,14 @@ Starts a Flask web application
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """
     returns Hello HBNB
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -32,6 +34,7 @@ def c(text):
     """
     return 'C {}'.format(text.replace('_', ' '))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
@@ -40,6 +43,8 @@ def pythoniscool(text='is cool'):
     of the text variable replacing underscore _
     symbols with a space
     """
+
+
     return 'Python {}'.format(text.replace('_',' '))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
