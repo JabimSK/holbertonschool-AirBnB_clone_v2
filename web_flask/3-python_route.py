0c2d1df542fb9b2f0cd2e5f0779a7df79e611597
JabimSK <6357@holbertonstudents.com>
2023-08-14 17:01:21 -0700

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -45,6 +45,6 @@ def pythoniscool(text='is cool'):
     """
 
 
-    return 'Python {}'.format(text.replace('_',' '))
+return 'Python {}'.format(text.replace('_',' '))
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
