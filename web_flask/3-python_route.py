bc2647fa080f3ec16714ac8a819171e5c04c89bb
JabimSK <6357@holbertonstudents.com>
2023-08-16 15:32:42 -0700

    1-hbnb_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,48 +1,51 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
-Starts a Flask web application
-
+imports
 """
-
 from flask import Flask
+
+
 app = Flask(__name__)
+"""
+Routes
+"""
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
-    """
-    returns Hello HBNB
-    """
-    return 'Hello HBNB!'
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    returns HBNB
-    """
-    return 'HBNB'
+"""
+HBNB
+"""
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """
-    display “C ” followed by the value
-    of the text variable replacing underscore
-    _ symbols with a space
-
-    """
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return ("HBNB")
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """
-    display “Python ”, followed by the value
-    of the text variable replacing underscore _
-    symbols with a space
-    """
-    return 'Python {}'.format(text.replace('_' , ' '))
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+"""
+C is fun
+"""
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    newtext = text.replace("_", " ")
+    return (f"C {newtext}")
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python(text):
+    text = text.replace("_", " ")
+    return (f"Python {text}")
+
+
+"""
+Define the host and port that the web app is listening
+"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
