5852d808d403cb692de019b17dddb50d6f0fbed3
JabimSK <6357@holbertonstudents.com>
2023-08-19 19:27:56 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,7 @@ class FileStorage:
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review   
+            'Review': Review
         }
         try:
             temp = {}
